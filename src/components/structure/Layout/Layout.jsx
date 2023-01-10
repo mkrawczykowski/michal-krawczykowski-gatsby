cff34c4127eb0f15e9a44e4a631cf7e3f660f8ad
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-10 22:22:49 +0100

    basic structure + Layout

---
+++ src/components/structure/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as styles from './Layout.module.scss';
+
+const Layout = ({children}) => {
+    return(
+        <main>
+            {children}
+        </main>
+    )
+}
+
+export default Layout;
---
+++ src/templates/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {graphql} from 'gatsby';
+import Layout from '../components/Layout/Layout';
+
+const PageTemplate = () => {
+    return(
+        <Layout>
+            <p>test</p>
+        </Layout>
+    )
+}
+
+export default PageTemplate;
