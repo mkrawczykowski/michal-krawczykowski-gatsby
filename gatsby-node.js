dd8845b07cbb20c31ba3017b55515be451badfec
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-15 15:22:02 +0100

    home page working without index.jsx

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,6 +13,12 @@ exports.createPages = async ({ actions, graphql }) =>{
     const pages = result.data.allWpPage.nodes;
 
     pages.forEach(page => {
+        // https://stackoverflow.com/questions/59868720/how-can-i-set-the-index-page-of-my-gatsby-site-to-be-one-of-the-dynamically-gene
+        // with 'uri' it would be '/start/'
+        if (page.slug==='start') {
+            page.slug = '/'    
+        }
+        
         actions.createPage(
             {
                 path: page.slug,
@@ -23,4 +29,14 @@ exports.createPages = async ({ actions, graphql }) =>{
             }
         )
     });
+
+    // actions.createPage(
+    //     {
+    //         path: '/',
+    //         component: require.resolve('./src/templates/page.jsx'),
+    //         context: {
+    //             id: page.id,
+    //         }
+    //     }
+    // )
 }
