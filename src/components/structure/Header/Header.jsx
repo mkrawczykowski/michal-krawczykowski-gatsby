a53f2800460b23d3063b7a00acdab0b0589f6d41
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-15 15:22:17 +0100

    Link component in Header

--- src/components/structure/Header/Header.jsx
+++ src/components/structure/Header/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import {Link} from 'gatsby';
 import * as styles from './Header.module.scss';
 
 const Header = () =>{
     return(
         <header>
             <nav>
-            <img src="" className={logo}></img>
+            <img src=""></img>
             <ul>
-                <li><a>link</a></li>
-                <li><a>link</a></li>
-                <li><a>link</a></li>
-                <li><a>link</a></li>
+                <li><Link to="/"></Link></li>
+                <li><Link to="/blog"></Link></li>
+                <li><Link to="/contact"></Link></li>
             </ul>
             <button>Contact</button>
             <div className="hamburger"></div>
