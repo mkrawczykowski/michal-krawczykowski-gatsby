cb8091c1f95e1cc45f8114aa203cd159bfdcf246
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-16 14:58:44 +0100

    reactifySplitClass in Grid function amost working

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {graphql} from 'gatsby';
 import Layout from '../components/structure/Layout/Layout';
+import {Container, Col, Row} from '../components/structure/Grid/Grid';
 
 export const query = graphql`
     query($id: String!){
@@ -16,10 +17,16 @@ const PageTemplate = ({data}) => {
     const slug = data.wpPage.slug;
     return(
         <Layout>
-            <>
-                {title}
-            </>
-
+            <Container>
+                <Row>
+                    <Col classes="col-6 col-lg-12 col-xl-7">{title}</Col>
+                    {/* <Col classes="col-6 col-lg-12 col-xxl-7">
+                        <Row>
+                            <Col classes="col-6">asd</Col>
+                        </Row>
+                    </Col> */}
+                </Row>
+            </Container>
         </Layout>
     )
 }
