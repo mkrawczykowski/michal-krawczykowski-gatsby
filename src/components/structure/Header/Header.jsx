4fdea507300f67b38842b81234b39bbe4271ff5d
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-19 09:46:56 +0100

    some styling for Header

--- src/components/structure/Header/Header.jsx
+++ src/components/structure/Header/Header.jsx
@@ -6,17 +6,17 @@ import * as styles from './Header.module.scss';
 
 const Header = () => {
     return(
-        <header>
+        <header className={styles.header}>
             <Container>
-                <nav>
+                <nav className={styles.header__mainNav}>
                     <Link to="/"><StaticImage src="../../../images/krawczykowski-logo.svg" alt="Michael Krawczykowski's blog - logo" /></Link>
                     {/* <ul>
                         <li></li>
                         <li><Link to="/blog">Blog</Link></li>
                         <li><Link to="/contact">My websites</Link></li>
                     </ul> */}
-                    <button>Contact</button>
-                    <div className="hamburger"></div>
+                    <button className={styles.header__contactButton}>Contact</button>
+                    {/* <div className="hamburger"></div> */}
                 </nav>    
             </Container>
 
