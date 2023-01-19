f071d3a42ebeb6d1a37bda223661be61874a43e0
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-19 09:29:26 +0100

    added logo

--- src/components/structure/Header/Header.jsx
+++ src/components/structure/Header/Header.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import {Container, Row, Col} from '../../structure/Grid/Grid';
+import {StaticImage} from 'gatsby-plugin-image';
 import * as styles from './Header.module.scss';
 
 const Header = () => {
     return(
         <header>
-            <nav>
-            <img src=""></img>
-            <ul>
-                <li><Link to="/"></Link></li>
-                <li><Link to="/blog"></Link></li>
-                <li><Link to="/contact"></Link></li>
-            </ul>
-            <button>Contact</button>
-            <div className="hamburger"></div>
-            </nav>
+            <Container>
+                <nav>
+                    <Link to="/"><StaticImage src="../../../images/krawczykowski-logo.svg" alt="Michael Krawczykowski's blog - logo" /></Link>
+                    {/* <ul>
+                        <li></li>
+                        <li><Link to="/blog">Blog</Link></li>
+                        <li><Link to="/contact">My websites</Link></li>
+                    </ul> */}
+                    <button>Contact</button>
+                    <div className="hamburger"></div>
+                </nav>    
+            </Container>
+
         </header>
     )
 }
