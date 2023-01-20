62e25419c9c714531e91bb84f0ca21fcbfb44185
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-20 15:17:13 +0100

    header styling + MainMenu

--- src/components/structure/Header/Header.jsx
+++ src/components/structure/Header/Header.jsx
@@ -3,6 +3,7 @@ import {Link} from 'gatsby';
 import {Container, Row, Col} from '../../structure/Grid/Grid';
 import {StaticImage} from 'gatsby-plugin-image';
 import * as styles from './Header.module.scss';
+import MainMenu from '../MainMenu/MainMenu';
 
 const Header = () => {
     return(
@@ -10,13 +11,7 @@ const Header = () => {
             <Container>
                 <nav className={styles.header__mainNav}>
                     <Link to="/"><StaticImage src="../../../images/krawczykowski-logo.svg" alt="Michael Krawczykowski's blog - logo" /></Link>
-                    {/* <ul>
-                        <li></li>
-                        <li><Link to="/blog">Blog</Link></li>
-                        <li><Link to="/contact">My websites</Link></li>
-                    </ul> */}
-                    <button className={styles.header__contactButton}>Contact</button>
-                    {/* <div className="hamburger"></div> */}
+                    <MainMenu></MainMenu>
                 </nav>    
             </Container>
 
