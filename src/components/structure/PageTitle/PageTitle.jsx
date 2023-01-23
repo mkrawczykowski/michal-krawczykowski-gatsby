a39951dc0b169356a8d14f85ef5f5edd152202db
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-23 08:57:29 +0100

    Col structure for PageTitle

--- src/components/structure/PageTitle/PageTitle.jsx
+++ src/components/structure/PageTitle/PageTitle.jsx
@@ -7,10 +7,12 @@ const PageTitle = ({page}) => {
     return(
         <Container>
             <Row>
-                <Col>
+                <Col classes="col-1"></Col>
+                <Col classes="col-10">
                     {page.titleACF ? <h1>{page.titleACF}</h1> : <h1>{page.title}</h1>}
                     {page.descriptionACF ? <h3>{page.descriptionACF}</h3> : null}
                 </Col>
+                <Col classes="col-1"></Col>
             </Row>
         </Container>
     )
