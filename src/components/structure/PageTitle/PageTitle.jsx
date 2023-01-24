ceae1c1c46537c76d8706cd63eb7388126989a0f
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-24 21:19:07 +0100

    PageTitle with working Columns

--- src/components/structure/PageTitle/PageTitle.jsx
+++ src/components/structure/PageTitle/PageTitle.jsx
@@ -7,12 +7,12 @@ const PageTitle = ({page}) => {
     return(
         <Container>
             <Row>
-                <Col classes="col-1"></Col>
-                <Col classes="col-10">
+                <Col classes="col-xs-2 col-md-8 col-2xl-1">s</Col>
+                <Col classes="col-xs-2 col-md-8 col-2xl-10">
                     {page.titleACF ? <h1>{page.titleACF}</h1> : <h1>{page.title}</h1>}
                     {page.descriptionACF ? <h3>{page.descriptionACF}</h3> : null}
                 </Col>
-                <Col classes="col-1"></Col>
+                <Col classes="col-xs-2 col-md-8 col-2xl-1">s</Col>
             </Row>
         </Container>
     )
