942f3ae74d43866829793abdbf08ada4e3c8d70f
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-28 15:14:18 +0100

    WYSIWYG with content and columns

--- src/components/sections/WYSIWYG/WYSIWYG.jsx
+++ src/components/sections/WYSIWYG/WYSIWYG.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import {graphql} from 'gatsby';
 import * as styles from './WYSIWYG.module.scss';
+import {Container, Col, Row} from '../../structure/Grid/Grid';
 
 const WYSIWYG = ({data}) => {
     const wysiwyg = data.wysiwyg;
 
     return(
         <section className={styles.WYSIWYG}>
-            <div dangerouslySetInnerHTML={{__html: wysiwyg}}></div>
+            <Container>
+                <Row>
+                    <Col classes="col-xs-2 col-sm-1 col-2xl-1"></Col>
+                    <Col classes="col-xs-2 col-sm-6 col-2xl-10">
+                        <div dangerouslySetInnerHTML={{__html: wysiwyg}}></div>
+                    </Col>
+                    <Col classes="col-xs-2 col-sm-1 col-2xl-1"></Col>
+                </Row>
+            </Container>
         </section>
     )
 }
