e6138cedf021c1d1c4bb346997312b10c0e9c98d
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-01-30 20:03:29 +0100

    RM

--- src/components/sections/_FlexibleSection/FlexibleSection.jsx
+++ src/components/sections/_FlexibleSection/FlexibleSection.jsx
@@ -5,99 +5,34 @@ const FlexibleSection = ({section}) => {
     let sections = [];
     const generateNestedSections = () => {
         if (section.sections){
-            console.log('section');
-            console.log(section);
-            console.log('section.sections');
-            console.log(section.sections);
             section.sections.map(nestedSection => {
             sections.push(<FlexibleSection section={nestedSection} />) 
-            console.log('sections')
-            console.log(sections)
-            console.log('======================')
         });
         }
 
     }
     
-
     const printSections = (sections) => {
         return(
-        sections.map(section => {
-            return section;
-        })    
+            sections.map(section => {
+                return section;
+            })    
         )
-        
     }
-
 
     let sectionName = section.fieldGroupName.substring(section.fieldGroupName.lastIndexOf('_')+1, section.fieldGroupName.length);
 
-    // return(
-    //     <>
-    //     <pre>{JSON.stringify(section, null, 2)}</pre>
-    //     {
-    //       sectionName
-    //     }
-    //     </>
-    // )
+    switch(sectionName){
+        case 'Wysiwyg':
+            sections.push(<WYSIWYG data={section} />)
+        case 'Section':
+            generateNestedSections();
+            break;
+    }  
 
-    // switch(sectionName){
-    //             case 'Wysiwyg':
-    //             return <WYSIWYG data={section} />
-    //             case 'Section':
-                
-    //         }
-    
-    //=========================== BELOW Kind of works ========================
-// return(
-//     <>  
-            
-//         <WYSIWYG data={section} />
-//         {console.log(nestedSections)}
-//         {nestedSections? nestedSections : null}
-//     </>
-// )
-
-
-     switch(sectionName){
-            case 'Wysiwyg':
-                sections.push(<WYSIWYG data={section} />)
-            case 'Section':
-                generateNestedSections();
-                break;
-        }  
-
-  
-        return (
-
-                    printSections(sections)
-            
-        )
-
-
-
-
-
-
-
-
-    // let sectionName = fieldGroupName.substring(fieldGroupName.lastIndexOf('_')+1, fieldGroupName.length);
-
-    // (() => {
-    //     switch(sectionName){
-    //         case 'Wysiwyg':
-    //         return <WYSIWYG data={sectionData} />
-    //         case 'Section':
-                
-    //             const flexibleSections = sectionData.sections;
-    
-    //                 flexibleSections?.map((flexibleSection, flexibleSectionIndex) => {
-    //                     const {fieldGroupName, ...sectionData} = flexibleSection;
-    //                     console.log(flexibleSection)
-    //                     return <FlexibleSections fieldGroupName={fieldGroupName} sectionData={sectionData}></FlexibleSections>
-    //                 })
-    //     }
-    // })()
+    return (
+        printSections(sections)
+    )
 }
 
 export default FlexibleSection;
--- src/components/sections/_FlexibleSections/FlexibleSections.jsx
+++ src/components/sections/_FlexibleSections/FlexibleSections.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 import FlexibleSection from '../_FlexibleSection/FlexibleSection';
 
 const FlexibleSections = ({sections}) => {
-    console.log('=sections=')
-            console.log(sections)
-            console.log('==')
     return(
         sections.map(section => (
             <section>
-            <FlexibleSection section={section} />    
+                <FlexibleSection section={section} />    
             </section>
-            
-            
         ))    
     )
-    
 }
 
 export default FlexibleSections;
