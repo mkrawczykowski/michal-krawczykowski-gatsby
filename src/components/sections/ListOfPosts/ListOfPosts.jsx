092a40cbdfbf27f932ad028291d7b0ad6b3601bb
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-02-07 17:25:45 +0100

    TheLoop works (without the Post)

--- src/components/sections/ListOfPosts/ListOfPosts.jsx
+++ src/components/sections/ListOfPosts/ListOfPosts.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 import {useStaticQuery, graphql} from 'gatsby';
 import {Container, Row, Col} from '../../structure/Grid/Grid';
-import {isInCategory, getPosts} from '../../../inc/helpers';
+import TheLoop from '../../partials/TheLoop/TheLoop';
 import Card from '../../partials/Card/Card';
 
 const ListOfPosts = ({data, postCategories, postID}) => {
-    const allPostsQuery = useStaticQuery(graphql`
+    const query = useStaticQuery(graphql`
         query blogPosts{
-            allWpPost(limit: 1000) {
-                nodes {
-                    id
-                    date
-                    title
-                    PageTitle {
-                        pageTitle
-                    }
-                    PostACFData {
-                        postExcerpt
-                    }
-                    categories {
-                        nodes {
-                            id
-                        }
-                    }
-                }
-            }
             allWpCategory {
                 nodes {
                     id
@@ -35,30 +17,22 @@ const ListOfPosts = ({data, postCategories, postID}) => {
 
     const dSourceOfPosts = data.sourceOfPosts;
     const dSourceCategories = data.sourceCategories;
-    let dPostAllCategories = postCategories.map(dPostAllCategory => dPostAllCategory.id);
     const dPostID = postID;
     let sourceCategories = [];
-    const qAllPosts = allPostsQuery.allWpPost.nodes;
-    const qAllCategories = allPostsQuery.allWpCategory.nodes.map(qAllCategory => {
+    const qAllCategories = query.allWpCategory.nodes.map(qAllCategory => {
         return qAllCategory.id;
     });
-
-    console.log('getPosts')
-    console.log(getPosts)
 
     switch (dSourceOfPosts){
         case 'Selected category(ies)':
             console.log('selected cats')
-            sourceCategories = dSourceCategories.map(dSourceCategory => dSourceCategory.id);
+            if (dSourceCategories){
+                sourceCategories = dSourceCategories.map(dSourceCategory => dSourceCategory.id);
+            }
+            console.log('sourceCategories')
+            console.log(sourceCategories)
         break;
         case 'All categories':
-            console.log('all cats')
-            console.log('qAllCategories')
-            console.log(qAllCategories)
-            console.log('dPostAllCategories')
-            console.log(dPostAllCategories)
-            console.log('dPostID')
-            console.log(dPostID)
             qAllCategories.forEach(qAllCategory => {
                 console.log('----qAllCategory----');
                 console.log(qAllCategory);
@@ -69,42 +43,17 @@ const ListOfPosts = ({data, postCategories, postID}) => {
         break;
         case 'Current post\'s category(ies)':
             console.log('current cats')
-            if (dPostAllCategories){
-                qAllCategories.forEach(qAllCategory => {
-                    console.log('----qAllCategory----');
-                    console.log(qAllCategory);
-                    if (isInCategory(dPostAllCategories, qAllCategory)){
-                        console.log('isInCategory(dPostAllCategories, qAllCategory)')
-                        console.log(isInCategory(dPostAllCategories, qAllCategory))
-                        sourceCategories.push(qAllCategory);
-                    }   
-                });
-            }
-            if (!dPostAllCategories){
-                sourceCategories = 0;
-            }
+            sourceCategories = postCategories?.map(dPostAllCategory => dPostAllCategory.id);
+            console.log('sourceCategories')
+            console.log(sourceCategories)
         break;
     }
-
-    let postsToPrint = [];
-    qAllPosts.forEach(qAllPost => {
-        let qAllPostCategories = qAllPost.categories.nodes.map(qAllPostcategory => {
-            return qAllPostcategory.id;
-        })
-        if (qAllPost.id != postID){
-        if (isInCategory(sourceCategories, qAllPostCategories)){
-            postsToPrint.push(qAllPost);
-        }
-        }
-    })
-    console.log('postsToPrint')
-    console.log(postsToPrint)
     return(
         <Container>
             <Row>
                 <Col classes="col-xs-2 col-sm-1">
                     {
-                        
+                        <TheLoop categories={sourceCategories} omitPostId={postID}></TheLoop>
                     }
                 </Col>
             </Row>
